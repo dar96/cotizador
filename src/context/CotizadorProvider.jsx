3c0c04d234c8ef645652174184362bf9c9b85e4f
dar96 <darielfriascastillo@gmail.com>
2022-03-17 11:44:06 +0100

    first commit

---
+++ src/context/CotizadorProvider.jsx
@@ -0,0 +1,92 @@
+import { useState,createContext } from "react";
+import { obtenerDiferenciaYear } from "../helpers";
+import { calcularMarca } from "../helpers";
+import { calcularPlan } from "../helpers";
+import { formatearDinero } from "../helpers";
+
+const CotizadorContext = createContext()
+
+const CotizadorProvider = ({children})=>{
+
+    const [datos, setDatos] = useState({
+        marca:"",
+        year:"",
+        plan:""
+    })
+
+    const [error, setError]=useState("")
+    const [resultado, setResultado]=useState(0)
+    const [cargando, setCargando]=useState(false)
+
+    const handleChangeDatos=e=>{
+        setDatos({
+            ...datos,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    const cotizarSeguro=()=>{
+        //Una base
+        let resultado = 2000
+
+
+        //Obtener diferencia de años
+        const diferencia = obtenerDiferenciaYear(datos.year)
+       
+
+        //Hay que restar el 3% por cada año
+        resultado-=((diferencia*3)*resultado)/100
+        
+
+
+        //Americano 15%
+        //Europeo 30%
+        //Asiático 5%
+        resultado*=calcularMarca(datos.marca)
+        
+        
+
+        //Básico 20%
+        //Completo 50%
+        resultado *= calcularPlan(datos.plan)
+        
+        //resultado=resultado.toFixed(2)
+        
+        //Formatear dinero
+        resultado=formatearDinero(resultado)
+
+        setCargando(true)
+
+        setTimeout(()=>{
+            setResultado(resultado)
+            setCargando(false)
+        },1500)
+        setResultado(resultado)
+
+
+    }
+
+    
+
+    return(
+        <CotizadorContext.Provider
+            value={{
+                datos,
+                handleChangeDatos,
+                error,
+                setError,
+                cotizarSeguro,
+                resultado,
+                cargando
+            }}
+        >
+            {children}
+        </CotizadorContext.Provider>
+    )
+}
+
+export {
+    CotizadorProvider
+}
+
+export default CotizadorContext
